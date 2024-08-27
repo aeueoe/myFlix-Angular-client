c3a91afd36bf365dbbe77098b5f0439b9fe3f4fb
aeueoe <drom.jul@gmail.com>
2024-08-27 16:57:58 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    // add other components here
+  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
